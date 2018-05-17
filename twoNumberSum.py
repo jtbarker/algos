a21b1886afcac8f2b0e07a6810edda1fdf1cb5f6
jtbarker <jonathan.t.barker@gmail.com>
2018-05-17 14:23:09 -0500

    added twosum

---
+++ twoNumberSum.py
@@ -0,0 +1,15 @@
+# time O(n log n) space O(log n)
+def twoNumberSum(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+    return []
+
